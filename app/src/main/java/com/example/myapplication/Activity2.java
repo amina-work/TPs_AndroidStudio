8df3e6d1d3d5f801dd92c84d279cb9cd4d4f037e
amina-work <mina.works28@gmail.com>
2023-02-27 11:56:10 +0100

    errors with initialatation

--- app/src/main/java/com/example/myapplication/Activity2.java
+++ app/src/main/java/com/example/myapplication/Activity2.java
@@ -25,36 +25,42 @@ public class Activity2 extends AppCompatActivity {
         sub = (Button)findViewById(R.id.sub);
         result = (TextView)findViewById(R.id.result);
 
-        Intent intent = getIntent();
-        int num1 = intent.getIntExtra("n1", 0); //default value is necessary for code to work
-        int num2 = intent.getIntExtra("n2", 0);
-
+        //Intent intent = getIntent();
+        //int num1 = intent.getIntExtra("n1", 0); //default value is necessary for code to work
+        //int num2 = intent.getIntExtra("n2", 0);
+        Bundle extras = getIntent().getExtras();
+        int num1 = extras.getInt("n1", 0);
+        int num2 = extras.getInt("n2", 0);
 
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int sum = num1 + num2;
-                result.setText("La Reponse:" + String.valueOf(sum));
+                //result.setText("La Reponse:" + String.valueOf(sum));
+                openActivityMain();
             }
         });
         mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int sum = num1 * num2;
-                result.setText("La Reponse:" + String.valueOf(sum));
+                //result.setText("La Reponse:" + String.valueOf(sum));
+                openActivityMain();
             }
         });
         sub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 int sum = num1 - num2;
-                result.setText("La Reponse:" + String.valueOf(sum));
+                //result.setText("La Reponse:" + String.valueOf(sum));
+                openActivityMain();
             }
         });
     }
 
     public void openActivityMain() {
         Intent intent = new Intent(Activity2.this, MainActivity.class);
+        int sum = Integer.parseInt(sum.toString());
         startActivity(intent);
     }
 }
