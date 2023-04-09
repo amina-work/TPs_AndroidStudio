2fdbee15988abf9441d4d9b71ed9b2d779d9135c
amina-work <mina.works28@gmail.com>
2023-04-09 22:32:37 +0100

    intializing TP3 branch

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,50 +16,39 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView pr;
-    //private Button btn;
-    private EditText txt;
-    private TextView rst;
-    private ProgressBar bar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        pr = (TextView)findViewById(R.id.pr);
-        //btn = (Button)findViewById(R.id.btn1);
-        rst = (TextView)findViewById(R.id.result);
-        txt = (EditText)findViewById(R.id.text1);
-        bar = (ProgressBar)findViewById(R.id.bar);
-        bar.setVisibility(View.GONE);
+        ExampleAsync task = new ExampleAsync();
+        task.execute(10);
     }
 
-    public void myThread(View v){
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for(int i=0;i<=40; i++){
-                    try{
-                        Thread.sleep(100);
-                    } catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                    final int valeur = i;
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            pr.setText(valeur + "%");
-                            bar.setVisibility(View.VISIBLE);
-                            bar.setProgress(valeur);
-                        }
-                    });
-                }
-                bar.setVisibility(View.INVISIBLE);
-                String text = txt.getText().toString();
-                rst.setText(text);
-            }
-        });
-        t.start();
+    private static class ExampleAsync extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // ...
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            // ...
+            return "Finished!";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            // ...
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            super.onPostExecute(string);
+            // ...
+        }
     }
+
 }
